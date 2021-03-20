af0855885e923810d92ee4ec746125279c09ad7c
Malcolm Macleod <malcolm@macleodhome.com>
2021-03-20 23:31:57 +0000

    7.4 - custom useField hook

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import {
   Switch, Route, Link,
-  useRouteMatch, useParams, useHistory
+  useRouteMatch, useParams
 } from 'react-router-dom'
 import Anecdote from './Anecdote'
 import Notification from './Notification'
+import CreateNew from './CreateNew'
 
 
 const Menu = () => {
@@ -51,49 +52,6 @@ const Footer = () => (
   </div>
 )
 
-const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
-  const history = useHistory()
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    props.addNew({
-      content,
-      author,
-      info,
-      votes: 0
-    })
-
-    history.push("/");
-
-  }
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-        </div>
-        <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-
-}
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
---
+++ routed-anecdotes/src/CreateNew.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import  {useField} from './hooks'
+
+const CreateNew = ({addNew}) => {
+
+  const history = useHistory()
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
+
+    history.push("/");
+
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content}/>
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+
+}
+
+export default CreateNew
